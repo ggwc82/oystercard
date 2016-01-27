ed41d11e1268202a64d53a5fff88112318155b45
Matt <mattpgough@gmail.com>
2016-01-27 09:38:58 +0000

    Added min balance cheack, charge for journey & entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,15 @@
 class OysterCard
 
  MAXIMUM_BALANCE = 90
+ MINIMUM_BALANCE = 1
+ MINIMUM_CHARGE = 1
 
- attr_reader :balance
- attr_accessor :status
+ attr_reader :balance, :entry_station
+ attr_accessor :in_journey
 
- def initialize()
+ def initialize
    @balance = 0 
-   @status = false
+   @entry_station = nil
  end
 
  def top_up(amount)
@@ -15,20 +17,25 @@ class OysterCard
  	@balance += amount
  end
 
+ def touch_in(station)
+ 	raise "Insufficient balance" if @balance < MINIMUM_BALANCE
+ 	@entry_station = station
+ end
+
+ def touch_out
+ 	deduct(MINIMUM_CHARGE)
+    @entry_station = false
+ end
+
+ def in_journey?
+ 	!!@entry_station
+ end
+
+ private
+
  def deduct(amount)
  	@balance -= amount
  end
 
- def touch_in
- 	@status = true
- end
-
- def touch_out
-    @status = false
- end
-
- def in_journey?
-   @status
- end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,7 @@
 require 'oystercard'
 
 describe OysterCard do
+   let(:station){double :station}
   it 'has a balance of zero' do
   	expect(subject.balance).to eq(0)
   end
@@ -11,40 +12,61 @@ describe '#top_up' do
  end 
 end
 
-it 'raises an error when balance more than 90' do
+it 'raises an error when balance greater than limit' do
     maximum_balance = OysterCard::MAXIMUM_BALANCE
     subject.top_up(maximum_balance)
 	expect{ subject.top_up(1) }.to raise_error "error balance greater than #{maximum_balance}"
 end
 
  describe '#deduct' do
-  it 'deducts amount from balance' do
-    subject.top_up(20)
-  expect{subject.deduct(1)}.to change{subject.balance}.by -1
-  end
+  it 'deducts the minimum' do
+  min_charge= OysterCard::MINIMUM_CHARGE
+  subject.top_up(10)
+  subject.touch_in(station)
+  expect{subject.touch_out}.to change{subject.balance}.by -(min_charge)
+end
  end
  describe '#touch_in' do
   it 'it touches in' do
-  subject.touch_in
-  expect(subject.status).to eq(true)
+  subject.top_up(1)
+  subject.touch_in(station)
+  expect(subject).to be_in_journey
 end
 end
  describe '#touch_out' do
  it 'touches out' do
-  subject.touch_in
+  subject.top_up(1)
+  subject.touch_in(station)
   subject.touch_out
-  expect(subject.status).to eq(false)
+  expect(subject).not_to be_in_journey
 end
 end
 
  describe '#in_journey?' do
   it 'returns true when in journey' do
-    subject.touch_in
+    subject.top_up(1)
+    subject.touch_in(station)
     expect(subject).to be_in_journey
   end
   it 'returns false initially' do
-    expect(subject).not_to be_in_journey
-  end
+     subject.top_up(1)
+     expect(subject).not_to be_in_journey
+end
+
+  it 'returns error:insufficient balance' do
+    expect{subject.touch_in(station)}.to raise_error "Insufficient balance"
+end
+end
+
+describe 'returns entry station' do
+  it 'remembers entry station' do
+     subject.top_up(10)
+     subject.touch_in(station)
+     expect(subject.entry_station).to eq station
+   end
+ end
+
+
 end
 
 
@@ -60,4 +82,5 @@ end
 
 
 
-end
+
+
