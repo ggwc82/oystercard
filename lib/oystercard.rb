39413004e163dcb52f3cc9fe3413c7f37e162d5e
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-01-28 14:45:10 +0000

    Removed redundant attr_reader

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,11 @@
 
-
 class OysterCard
 
  MAXIMUM_BALANCE = 90
  MINIMUM_BALANCE = 1
  MINIMUM_CHARGE = 1
 
- attr_reader :balance, :entry_station,  :previous_journeys, :journeys
+ attr_reader :balance, :previous_journeys, :journeys
  
  def initialize
    @balance = 0
@@ -22,30 +21,30 @@ class OysterCard
  def touch_in(entry_station)
  	raise 'Balance too low.' if below_min_balance?
   @previous_journeys.merge!({entry_station: entry_station})
- end
+end
 
- def touch_out(exit_station)
- 	deduct(MINIMUM_CHARGE)
+def touch_out(exit_station)
+  deduct(MINIMUM_CHARGE)
   @previous_journeys.merge!({exit_station: exit_station})
   add_journey_to_array
   @previous_journeys = {}
- end
+end
 
- private
+private
 
- def add_journey_to_array
+def add_journey_to_array
   @journeys << @previous_journeys
- end
+end
 
- def deduct(amount)
- 	@balance -= amount
- end
+def deduct(amount)
+  @balance -= amount
+end
 
- def exceed_balance?(amount)
+def exceed_balance?(amount)
   balance + amount > MAXIMUM_BALANCE
- end
+end
 
- def below_min_balance?
+def below_min_balance?
   balance < MINIMUM_BALANCE
- end
+end
 end
